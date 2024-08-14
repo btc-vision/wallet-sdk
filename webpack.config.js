707590c08e29d61ed5e5b1aef10ab78dfc2a90e1
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-13 21:01:47 -0400

    added missing polyfi

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-    fallback: {
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      zlib: require.resolve("browserify-zlib"),
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  experiments: {
-    asyncWebAssembly: true,
-  },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        fallback: {
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve('stream-browserify'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            zlib: require.resolve('browserify-zlib'),
+            vm: require.resolve('vm-browserify')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    experiments: {
+        asyncWebAssembly: true
+    }
 };
